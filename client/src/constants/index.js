f92f9b96bb2f5a7c7f5cff1d2d0cb7f384315aab
milankatira <milankatira26@gmail.com>
2022-12-16 12:07:26 +0530

    complete

---
+++ client/src/constants/index.js
@@ -0,0 +1,19 @@
+import { createCampaign, dashboard, payment, profile, withdraw } from '../assets';
+
+export const navlinks = [
+  {
+    name: 'dashboard',
+    imgUrl: dashboard,
+    link: '/',
+  },
+  {
+    name: 'campaign',
+    imgUrl: createCampaign,
+    link: '/create-campaign',
+  },
+  {
+    name: 'profile',
+    imgUrl: profile,
+    link: '/profile',
+  },
+];
